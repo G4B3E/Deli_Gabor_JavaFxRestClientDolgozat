fccf5d1b597d1978bf13ed41c3ed8f19a2def8f4
Deli Gábor <96292781+G4B3E@users.noreply.github.com>
2022-12-04 21:14:16 +0100

    commit

--- src/main/java/com/example/deli_gabor_javafxrestclientdolgozat/CreateMovieController.java
+++ src/main/java/com/example/deli_gabor_javafxrestclientdolgozat/CreateMovieController.java
@@ -1,5 +1,23 @@
 package com.example.deli_gabor_javafxrestclientdolgozat;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
 public class CreateMovieController {
 
+    @FXML
+    private TextField NameField;
+    @FXML
+    private TextField PrizeField;
+    @FXML
+    private Spinner<Integer> YearField;
+    @FXML
+    private Button SubmitButton;
+
+    @FXML
+    public void SubmitClick(ActionEvent actionEvent) {
+    }
 }
